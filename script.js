ef8877af9eeb729715e64d0433aaaf5f1f62da94
Vinicius Zuin <92578179+viniciuszuin@users.noreply.github.com>
2024-07-08 16:39:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    alert('Mensagem enviada com sucesso!');
+    
+    // Aqui você pode adicionar o código para enviar os dados do formulário ao servidor
+});
